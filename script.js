248cfa523f9e51a071175deaf364a6c1faf83e78
nitinmmmut <NITINRAJ827@GMAIL.COM>
2023-01-23 14:08:06 +0530

    done

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
-function palindrome(str){
+// function palindrome(str){
 
+// }
+// module.exports = palindrome
+function Palindrome(str) {
+    return str === str.split('').reverse().join('');
+//     The split('') method is used to convert the string into an array of characters.
+// The reverse() method is used to reverse the order of the characters in the array.
+// The join('') method is used to convert the reversed array of characters back into a string.
 }
-module.exports = palindrome
+console.log(Palindrome("madam")); // Output: true
+console.log(Palindrome("hello")); // Output: false
